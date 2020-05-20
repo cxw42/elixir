7b1d8496fad0804009a2a1675f40b23903558275
Chris White <cxwembedded@gmail.com>
2020-05-19 21:09:04 -0400

    web.py: resolve path to __file__
    web.py finds its template directory with respect to __file__.  If
    __file__ is a symlink, resolve it.  This lets web.py work in a
    t/interact.pl session.

--- http/web.py
+++ http/web.py
@@ -353,7 +353,7 @@ if status == 404:
     realprint('Status: 404 Not Found')
 
 import jinja2
-loader = jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__), '../templates/'))
+loader = jinja2.FileSystemLoader(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../templates/'))
 environment = jinja2.Environment(loader=loader)
 template = environment.get_template('layout.html')
 
